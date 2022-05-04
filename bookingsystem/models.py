8dfea11c3757ab109cf0fbc66eeac91896bfc94d
Blush Audio Dev <blushaudio@gmail.com>
2022-05-04 23:23:21 +0100

    Database Migrations
    Created a model an migrated to the database

--- bookingsystem/models.py
+++ bookingsystem/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    timebooked = models.DateTimeField(auto_now_add=True)
+    # starttime = models.DateTimeField()
+    # endtime = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title

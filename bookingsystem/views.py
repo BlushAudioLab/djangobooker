ec8a9be0acbe2cbbeaa7f851396baf68324be594
Blush Audio Dev <blushaudio@gmail.com>
2022-05-05 19:12:25 +0100

    Templates Displaying Static Data

--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def say_hello(request):
-    return render (request,'hello.html',{'name': 'David'})
+events = [
+    {
+        'title': 'Mixing Session 2',
+        'owner': 'Baloo',
+        'timebooked': '2nd April, 1987',
+    },
+    {
+        'title': 'Big Drum Time',
+        'owner': 'Greg the Man',
+        'timebooked': '5th May, 2022',
+    }
+]
+
+
+def home(request):
+    context = {
+        'events': events
+    }
+    return render (request,'home.html', context)
+
+
+def about(request):
+    return render (request,'about.html', {'name': 'Emily'})

6db4e95e1c015057017037fed3abb1d7a1e00426
Blush Audio Dev <blushaudio@gmail.com>
2022-05-11 21:53:43 +0100

    Login/Logout Basic Routing + Users App

--- djangobooker/settings.py
+++ djangobooker/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookingsystem',
+    'bookingsystem.apps.BookingsystemConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'debug_toolbar',
 ]
 
@@ -128,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- djangobooker/urls.py
+++ djangobooker/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 from django.urls import path, include
 import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookingsystem/', include('bookingsystem.urls')),
+    path('', include('bookingsystem.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='bookingsystem-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='bookingsystem-logout'),
     path('__debug__', include(debug_toolbar.urls))
 ]
 

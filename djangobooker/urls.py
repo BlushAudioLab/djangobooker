41f3bdd95956b65a7f31421b95d19b95348397e9
Blush Audio Dev <blushaudio@gmail.com>
2022-05-13 21:44:29 +0100

    User Profile Creation W/ Picture

--- djangobooker/urls.py
+++ djangobooker/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import debug_toolbar
 
 urlpatterns = [
@@ -26,5 +28,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='bookingsystem-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='bookingsystem-logout'),
     path('__debug__', include(debug_toolbar.urls))
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
